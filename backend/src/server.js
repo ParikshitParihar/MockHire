65fdf05fb3c05e4210581808257a42b36625f776
parikshit <parikshitparihar04@gmail.com>
2026-02-20 13:05:22 +0530

    db-setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const { ENV } = require("./lib/env");
+const { connectDB } = require("./lib/db");
 
 // const PORT = 3000;
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
-
 app.get("/", (req,res)=>{
     res.status(200).json({msg:"Success from api"})
 })
 
-app.listen(ENV.PORT, () => {
-    console.log("Server is running on PORT", ENV.PORT);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {console.log("Server is running on PORT", ENV.PORT);});
+    } catch (error) {
+        console.log("Error starting the server",error);
+    }
+}
+
+startServer();
